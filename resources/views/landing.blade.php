abdddc9a386fb98a666b8d445bc97b2332fd64c4
akira4n <yobiijo94@gmail.com>
2026-01-22 20:27:40 +0700

    fix: fix error vite

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Oprec INTEL 2026</title>
-    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+    @vite(['resources/js/app.jsx'])
 </head>
 
 <body class="bg-gray-100">
