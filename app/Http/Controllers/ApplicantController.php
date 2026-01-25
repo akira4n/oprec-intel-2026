c06923fc8512702606b15c864b3364a9b8b11a48
akira4n <yobiijo94@gmail.com>
2026-01-25 12:21:56 +0700

    feat: tambah request yang dibutuhkan di controller

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -17,6 +17,8 @@ class ApplicantController extends Controller
         $applicant = Applicant::where('user_id', $user->id)->select([
             'id',
             'user_id',
+            'major',
+            'batch',
             'divisi_satu',
             'divisi_dua',
             'alasan_utama',
@@ -24,6 +26,10 @@ class ApplicantController extends Controller
             'alasan_dua',
             'path_tugas_satu',
             'path_tugas_dua',
+            'path_tiktok',
+            'path_instagram',
+            'path_pamflet',
+            'path_twibbon',
             'created_at'
         ])->first();
 
@@ -46,6 +52,8 @@ class ApplicantController extends Controller
         }
 
         $validated = $request->validate([
+            'major' => 'required|in:si,sk,ti,ka,mi,tk',
+            'batch' => 'required|in:2024,2025',
             'divisi_satu' => 'required|in:hrd,pr,mulmed,arrait,scrabble,newscasting,debate,toastmaster',
             'divisi_dua' => 'nullable|in:hrd,pr,mulmed,arrait,scrabble,newscasting,debate,toastmaster',
             'alasan_utama' => 'required|string|min:3|max:10000',
@@ -53,9 +61,25 @@ class ApplicantController extends Controller
             'alasan_dua' => 'nullable|string|max:10000',
             'file_tugas_satu' => 'required|file|mimes:pdf,zip,rar|max:5120',
             'file_tugas_dua' => 'nullable|file|mimes:pdf,zip,rar|max:5120',
+
+            'file_tiktok' => 'required|image|mimes:jpg,jpeg,png,webp|max:2048',
+            'file_instagram' => 'required|image|mimes:jpg,jpeg,png,webp|max:2048',
+            'file_pamflet' => 'required|image|mimes:jpg,jpeg,png,webp|max:2048',
+            'file_twibbon' => 'required|image|mimes:jpg,jpeg,png,webp|max:2048',
         ]);
 
+        if ($request->divisi_satu === $request->divisi_dua) {
+            return back()->withErrors([
+                'divisi_dua' => 'Pilihan divisi tidak boleh sama'
+            ]);
+        }
+
         $pathSatu = $request->file('file_tugas_satu')->store('tugas_oprec', 'public');
+
+        $pathTiktok = $request->file('file_tiktok')->store('tugas_screenshoot', 'public');
+        $pathInstagram = $request->file('file_instagram')->store('tugas_screenshoot', 'public');
+        $pathPamflet = $request->file('file_pamflet')->store('tugas_screenshoot', 'public');
+        $pathTwibbon = $request->file('file_twibbon')->store('tugas_screenshoot', 'public');
 
         $pathDua = null;
         if ($request->hasFile('file_tugas_dua')) {
@@ -64,6 +88,9 @@ class ApplicantController extends Controller
 
         Applicant::create([
             'user_id' => $user->id,
+            'major' => $validated['major'],
+            'batch' => $validated['batch'],
+
             'divisi_satu' => $validated['divisi_satu'],
             'divisi_dua' => $validated['divisi_dua'] ?? null,
             'alasan_utama' => $validated['alasan_utama'],
@@ -71,6 +98,12 @@ class ApplicantController extends Controller
             'alasan_dua' => $validated['alasan_dua'] ?? null,
             'path_tugas_satu' => $pathSatu,
             'path_tugas_dua' => $pathDua,
+
+            'path_tiktok' => $pathTiktok,
+            'path_instagram' => $pathInstagram,
+            'path_pamflet' => $pathPamflet,
+            'path_twibbon' => $pathTwibbon,
+
             'status' => 'pending'
         ]);
 
